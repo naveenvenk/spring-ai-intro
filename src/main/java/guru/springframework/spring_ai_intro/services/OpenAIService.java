a22b27a4e875973f81ec3c0c691857e4aaea9f27
Naveen Krishnan <krish.venkat@outlook.com>
2024-05-29 05:27:36 -0400

    first code

---
+++ src/main/java/guru/springframework/spring_ai_intro/services/OpenAIService.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring_ai_intro.services;
+
+public interface OpenAIService {
+
+    public String getAnswer(String question );
+}
---
+++ src/test/java/guru/springframework/spring_ai_intro/services/OpenAIServiceImplTest.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring_ai_intro.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenAIServiceImplTest {
+
+    @Test
+    void getAnswer() {
+    }
+}
